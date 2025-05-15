747966862e89e8657b4a9d1217641fec1ecea496
Alexandra Musina <sasha.musina2003@gmail.com>
2025-05-15 19:01:48 +0300

    template 15.05

---
+++ template 15.05/template.hpp
@@ -0,0 +1,34 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#include <string>  // Для std::string
+
+enum class Color { red, green, orange };
+enum class Fruit { apple, orange, pear };
+
+template <typename T>
+class Traits;
+
+template<>
+class Traits<Color> {
+public:
+    static std::string name(int index) {
+        if(index == 0) return "red";
+        else if(index == 1) return "green";
+        else if(index == 2) return "orange";
+        else return "unknown";
+    }
+};
+
+template<>
+class Traits<Fruit> {
+public:
+    static std::string name(int index) {
+        if(index == 0) return "apple";
+        else if(index == 1) return "orange";
+        else if(index == 2) return "pear";
+        else return "unknown";
+    }
+};
+
+#endif
